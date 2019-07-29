4a5f7d1561d9096b0ac891410c57b7a97da09ab3
Loic Coyle <loic.coyle@hotmail.fr>
2019-07-29 12:41:10 +0200

    reworked study_params

--- pysixdesk/lib/study_params.py
+++ pysixdesk/lib/study_params.py
@@ -45,7 +45,7 @@ class StudyParams:
     The placeholders are split up again, into oneturn, sixtrack and
     and mask dicts for use in pysixdesk.
     To get the placeholder patterns for the mask file use self.madx_params.
-    To get the placeholder patterns for the onturn sixtrack file use
+    To get the placeholder patterns for the oneturn sixtrack job use
     self.oneturn_params.
     To get the placeholder patterns for the sixtrack file use
     self.sixtrack_params.
@@ -53,51 +53,53 @@ class StudyParams:
 
     def __init__(self, mask_path, fort_path=f'{PYSIXDESK_ABSPATH}/templates/fort.3'):
         self._logger = logging.getLogger(__name__)
+        # comment regexp
         self._reg_comment = re.compile(r'^(\s?!|\s?/).*', re.MULTILINE)
+        # placeholder pattern regexp
         self._reg = re.compile(r'%(?!FILE|%)([a-zA-Z0-9_]+/?)')
         self.fort_path = fort_path
         self.mask_path = mask_path
+        # initialize empty calculation queue
         self.calc_queue = []
-        # default parameters for the sixtack job
+        # default parameters for the sixtrack job
         self.defaults = dict([
-                             ("turnss", 1e4),
-                             ("nss", 60),  # I think this should be 60?
                              ("ax0s", 0.1),
                              ("ax1s", 0.1),
-                             ("imc", 1),
-                             ("iclo6", 2),
-                             ("writebins", 1),
-                             ("ratios", 1),
-                             ("Runnam", 'FirstTurn'),
-                             ("idfor", 0),
-                             ("ibtype", 0),
-                             ("ition", 0),
-                             ("ndafi", 1),
-                             ("tunex", 62.28),
-                             ("tuney", 60.31),
-                             ("inttunex", 62),
-                             ("inttuney", 60),
-                             ("toggle_post/", '/'),
-                             ("toggle_diff/", '/'),
-                             ("pmass", PROTON_MASS),
-                             ("emit_beam", 3.75),
-                             ("e0", 7000),
+                             ("BSEP", 25),
                              ("bunch_charge", 1.15E11),  # 2.2e11, 3.5e11 ?
-                             ("CHROM", 0),
+                             ("chromx", 2),  # 3.0?
+                             ("chromy", 2),   # 3.0?
                              ("chrom_eps", 0.000001),
                              ("dp1", 0.000001),
                              ("dp2", 0.000001),
-                             ("chromx", 2),  # 3.0?
-                             ("chromy", 2),   # 3.0?
+                             ("e0", 7000),
+                             ("emit_beam", 3.75),
+                             ("turnss", 1e4),
+                             ("ibtype", 0),
+                             ("iclo6", 2),
+                             ("idfor", 0),
+                             ("imc", 1),
+                             ("inttunex", 62),
+                             ("inttuney", 60),
+                             ("IOCT", -300),
+                             ("ition", 0),
                              ("length", 26658.864),
-                             ("XING", 255),
-                             ("rfvol", 16),
+                             ("ndafi", 1),
+                             ("nss", 60),  # I think this should be 60?
+                             ("pmass", PROTON_MASS),
                              ("PHI_IR5", 0),
                              ("PHI_IR1", 90.0),
-                             ("IOCT", -300),
-                             ("BSEP", 25),
+                             ("Runnam", 'FirstTurn'),
+                             ("rfvol", 16),
+                             ("ratios", 1),
+                             ("tunex", 62.28),
+                             ("tuney", 60.31),
+                             ("toggle_post/", '/'),
+                             ("toggle_diff/", '/'),
+                             ("writebins", 1),
+                             ("XING", 255),
                              ])
-
+        # phasespace params
         amp = [8, 10, 12]  # The amplitude
         self.phasespace_params = dict([
                                       ('amp', list(zip(amp, amp[1:]))),
@@ -105,13 +107,8 @@ class StudyParams:
                                       ('kmax', 5),
                                       ])
 
-        self._mask_ph = self.find_patterns(self.mask_path)
-        self._fort_ph = self.find_patterns(self.fort_path)
-        self.placeholders = FixedDict(**self.merge())
-
-        self._madx_params = None
-        self._oneturn_params = None
-        self._sixtrack_params = None
+        self.madx_params = FixedDict(**self.find_patterns(self.mask_path))
+        self.sixtrack_params = FixedDict(**self.find_patterns(self.fort_path))
 
     def extract_patterns(self, file):
         '''
@@ -155,76 +152,18 @@ class StudyParams:
             self._logger.debug(f'{k}: {v}')
         return out
 
-    def merge(self):
-        '''
-        Merges the mask and fort placeholders into a single dict.
-        '''
-        merged = OrderedDict()
-        for k, v in self._mask_ph.items():
-            merged[k] = v
-        for k, v in self._fort_ph.items():
-            if k not in merged.keys():
-                merged[k] = v
-        return merged
-
-    @property
-    def files(self):
-        '''
-        Creates an OrderedDict with the files in which each pattern occurs
-        '''
-        out = OrderedDict()
-        for k in self.placeholders.keys():
-            file = []
-            if k in self._mask_ph.keys():
-                file.append(self.mask_path)
-            if k in self._fort_ph.keys():
-                file.append(self.fort_path)
-            out[k] = file
-        return out
-
-    def _filter_file(self, file, filter_none=True):
-        '''
-        Selects for the placeholders occuring in file.
-        '''
-        filtered = OrderedDict()
-        for (k, v), f in zip(self.placeholders.items(), self.files.values()):
-            if filter_none and v is None:
-                continue
-            if file in f:
-                filtered[k] = v
-        return filtered
-
     @property
     def oneturn_params(self):
-        if self._oneturn_params is None:
-            sixtrack = self._filter_file(self.fort_path)
-            sixtrack['turnss'] = 1
-            sixtrack['nss'] = 1
-            sixtrack['Runnam'] = 'FirstTurn'
-            self._oneturn_params = sixtrack
-        return self._oneturn_params
-
-    @property
-    def sixtrack_params(self):
-        if self._sixtrack_params is None:
-            sixtrack = self._filter_file(self.fort_path)
-            # This is needed for compatibility with the way the pre_calc is done.
-            # it would be good to figure out a better to do it.
-            sixtrack.update(self.phasespace_params)
-            self._sixtrack_params = sixtrack
-        return self._sixtrack_params
-
-    @property
-    def madx_params(self):
-        if self._madx_params is None:
-            madx = self._filter_file(self.mask_path)
-            self._madx_params = madx
-        return self._madx_params
+        sixtrack = self.sixtrack_params.copy()
+        sixtrack['turnss'] = 1
+        sixtrack['nss'] = 1
+        sixtrack['Runnam'] = 'FirstTurn'
+        return sixtrack
 
     def add_calc(self, in_keys, out_key, fun):
         '''
         Add calculations to the calc queue. Any extra arguments of
-        fun can be given in the *args/**kwargs of self.calc.
+        fun can be given in the *args/**kwargs of the self.calc call.
         '''
         if isinstance(in_keys, list):
             self.calc_queue.append([in_keys, out_key, fun])
@@ -238,8 +177,8 @@ class StudyParams:
         at run time.
         '''
         for in_keys, out_key, fun in self.calc_queue:
-
-            inp = [self.sixtrack_params[k] for k in in_keys]
+            # get the input values with __getitem__
+            inp = [self.__getitem__(k) for k in in_keys]
 
             if isinstance(out_key, list):
                 out = fun(*inp, *args, **kwargs)
@@ -247,21 +186,43 @@ class StudyParams:
                     self.sixtrack_params[k] = out[i]
         return self.sixtrack_params
 
-    # set and get items like a dict
     def __repr__(self):
-        return self.placeholders.__repr__()
+        return '/n'.join([self.madx_params.__repr__(),
+                          self.sixtrack_params.__repr__(),
+                          self.phasespace_params.__repr__()])
 
+    # set and get items like a dict
     def __setitem__(self, key, val):
+        '''
+        Adds entry to the appropriate dictionnary.
+        '''
         if key in self.phasespace_params.keys():
             self.phasespace_params[key] = val
+        if key in self.madx_params.keys():
+            self.madx_params[key] = val
+        if key in self.sixtrack_params.keys():
+            self.sixtrack_params[key] = val
         else:
-            self.placeholders.__setitem__(key, val)
+            raise KeyError(f'"{key}" not in extracted placeholders.')
 
     def __getitem__(self, key):
-        return self.placeholders.__getitem__(key)
+        '''
+        Gets entry from the appropriate dictionnary.
+        '''
+        if key in self.phasespace_params.keys():
+            return self.phasespace_params[key]
+        if key in self.madx_params.keys():
+            return self.madx_params[key]
+        if key in self.sixtrack_params.keys():
+            return self.sixtrack_params[key]
 
     def update(self, *args, **kwargs):
-        self.placeholders.update(*args, **kwargs)
+        '''
+        Updates both dictionnaries.
+        '''
+        self.madx_params.update(*args, **kwargs)
+        self.sixtrack_params.update(*args, **kwargs)
+
 
 # for testing
 if __name__ == "__main__":
